3e84ffe88e469c833da588b7b40ce9747aed07b6
SergioRubioPracticaGit <srn20222@gmail.com>
2023-03-24 12:07:04 +0100

    SergioRubio_segundocommit

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,7 +2,7 @@ class TestCuentaBancaria
 {
 	public static void main(String args[])
 	{
-		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0,10);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
